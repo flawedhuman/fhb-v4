d97a51e767f457daeed1b6ce7dbf48d6de8f24c8
flawedhuman <110935159+flawedhuman@users.noreply.github.com>
2023-03-10 14:32:24 -0600

    adds rss, styles rss, fixes active album button style

--- src/components/RssFHB.vue
+++ src/components/RssFHB.vue
@@ -17,7 +17,7 @@
             },
             ParseJson(data){
                 console.log(data.items);
-                this.posts = data.items;
+                this.posts = data.items.slice(0, 3);
             }
         }
     }
@@ -27,14 +27,14 @@
     <div>
         <ul>
             <li v-for="post in posts">
-                <div class="p-3 bg-shark rounded-3xl m-5 font-semibold text-gray-300 text-center  w-3/4 mx-auto">
+                <div class="p-5 font-semibold text-white mx-auto">
                     <div class="inline-block text-left">
-                        <div class="text-xl pt-2" v-html="post.title"></div>
+                        <div class="text-5xl pt-2" style="line-height: 60px;" v-html="post.title"></div>
                         <div class="text-xl pt-2" v-html="post.description"></div>
                         <br />                        
                         <div class="grid grid-cols-2">
-                            <div class="text-left" style="color: #dabb1e">{{ post.pubDate }}</div>
-                            <div class="text-right"><a v-bind:href="post.link" class="text-gray-900">View on SubStack</a></div>
+                            <div class="text-left" style="color: #FFF500;">{{ post.pubDate }}</div>
+                            <div class="text-right"><a v-bind:href="post.link" class="text-white underline">View on SubStack</a></div>
                         </div>                        
                     </div>                    
                 </div>                    
