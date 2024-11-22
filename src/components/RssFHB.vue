f57f3279851f2750800e4cb340e5bf8570f41f7b
Matt Goddard <mgoddard1@gmail.com>
2024-11-22 14:05:37 -0600

    edits to rss component

--- src/components/RssFHB.vue
+++ src/components/RssFHB.vue
@@ -1,32 +1,48 @@
 <script>
-    export default {
-        data() {
-            return {
-                rssUrl: 'https://flawdhumanbeing.substack.com/feed',
-                posts: []
-            }
-        },
-        mounted(){
-            this.GetPosts();
-        },
-        methods:{
-            GetPosts(){
-                fetch(this.rssUrl)
-                .then(response => response.json())
-                .then((responseJson) => {this.ParseJson(responseJson);})
-            },
-            ParseJson(data){
-                console.log(data.items);
-                this.posts = data.items.slice(0, 10);
+export default {
+    data() {
+        return {
+            rssUrl: 'https://theflawedhumanbeings.substack.com/feed',
+            posts: []
+        }
+    },
+    mounted() {
+        this.getPosts();
+    },
+    methods: {
+        async getPosts() {
+            try {
+                // Fetch the RSS feed
+                const response = await fetch(this.rssUrl);
+                const text = await response.text();
+                
+                // Parse XML to DOM
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(text, 'text/xml');
+                
+                // Extract items
+                const items = xmlDoc.querySelectorAll('item');
+                
+                // Convert items to array of objects
+                this.posts = Array.from(items).slice(0, 12).map(item => ({
+                    title: item.querySelector('title')?.textContent || '',
+                    description: item.querySelector('description')?.textContent || '',
+                    pubDate: new Date(item.querySelector('pubDate')?.textContent || '').toLocaleDateString(),
+                    link: item.querySelector('link')?.textContent || ''
+                }));
+            } catch (error) {
+                console.error('Error fetching RSS feed:', error);
+                this.posts = [];
             }
         }
     }
+}
 </script>
 
 <template>
     <div>
         <ul>
-            <li v-for="post in posts">
+            <li v-for="post in posts" :key="post.link">
                 <div class="p-5 font-semibold text-white mx-auto">
                     <div class="inline-block text-left">
                         <div class="text-5xl pt-2" style="line-height: 60px;" v-html="post.title"></div>
@@ -34,7 +50,11 @@
                         <br />                        
                         <div class="grid grid-cols-2">
                             <div class="text-left" style="color: #FFF500;">{{ post.pubDate }}</div>
-                            <div class="text-right"><a v-bind:href="post.link" class="text-white underline">View on SubStack.com!</a></div>
+                            <div class="text-right">
+                                <a :href="post.link" class="text-white underline" target="_blank" rel="noopener noreferrer">
+                                    Follow along with The Flawed Human Beings on SubStack!
+                                </a>
+                            </div>
                         </div>                        
                     </div>                    
                 </div>                    
