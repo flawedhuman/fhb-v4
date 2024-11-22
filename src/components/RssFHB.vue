c1d2a5d4ccf33f39bc141eb2532c8fd66d430401
Matt Goddard <mgoddard1@gmail.com>
2024-11-22 14:53:51 -0600

    rss

--- src/components/RssFHB.vue
+++ src/components/RssFHB.vue
@@ -1,32 +1,53 @@
 <script>
-    export default {
-        data() {
-            return {
-                rssUrl: 'https://flawdhumanbeing.substack.com/feed',
-                posts: []
+import { ref, onMounted } from 'vue';
+
+export default {
+    setup() {
+        const rssUrl = 'https://theflawedhumanbeings.substack.com/feed';
+        const posts = ref([]);
+
+        const getPosts = async () => {
+            try {
+                // Using cors-anywhere as a proxy to avoid CORS issues
+                const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+                const response = await fetch(corsProxy + rssUrl);
+                const text = await response.text();
+                
+                // Parse XML to DOM
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(text, 'text/xml');
+                
+                // Extract items
+                const items = xmlDoc.getElementsByTagName('item');
+                
+                // Convert items to array of objects
+                posts.value = Array.from(items).slice(0, 12).map(item => ({
+                    title: item.getElementsByTagName('title')[0]?.textContent || '',
+                    description: item.getElementsByTagName('description')[0]?.textContent || '',
+                    pubDate: new Date(item.getElementsByTagName('pubDate')[0]?.textContent || '').toLocaleDateString(),
+                    link: item.getElementsByTagName('link')[0]?.textContent || ''
+                }));
+            } catch (error) {
+                console.error('Error fetching RSS feed:', error);
+                posts.value = [];
             }
-        },
-        mounted(){
-            this.GetPosts();
-        },
-        methods:{
-            GetPosts(){
-                fetch(this.rssUrl)
-                .then(response => response.json())
-                .then((responseJson) => {this.ParseJson(responseJson);})
-            },
-            ParseJson(data){
-                console.log(data.items);
-                this.posts = data.items.slice(0, 10);
-            }
-        }
+        };
+
+        onMounted(() => {
+            getPosts();
+        });
+
+        return {
+            posts
+        };
     }
+}
 </script>
 
 <template>
     <div>
         <ul>
-            <li v-for="post in posts">
+            <li v-for="post in posts" :key="post.link">
                 <div class="p-5 font-semibold text-white mx-auto">
                     <div class="inline-block text-left">
                         <div class="text-5xl pt-2" style="line-height: 60px;" v-html="post.title"></div>
@@ -34,11 +55,23 @@
                         <br />                        
                         <div class="grid grid-cols-2">
                             <div class="text-left" style="color: #FFF500;">{{ post.pubDate }}</div>
-                            <div class="text-right"><a v-bind:href="post.link" class="text-white underline">View on SubStack.com!</a></div>
+                            <div class="text-right">
+                                <a :href="post.link" class="text-white underline" target="_blank" rel="noopener noreferrer">
+                                    Follow along with The Flawed Human Beings on SubStack!
+                                </a>
+                            </div>
                         </div>                        
                     </div>                    
                 </div>                    
             </li>
         </ul>
-    </div>  
+    </div>
 </template>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+</style>
