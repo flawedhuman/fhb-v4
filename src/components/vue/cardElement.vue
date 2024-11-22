54a8e8ad07297be9cc47a3f69258a0eb77b7e1a0
Matt Goddard <mgoddard1@gmail.com>
2024-11-22 04:18:56 -0600

    npm fixes for stripe

---
+++ src/components/vue/cardElement.vue
@@ -0,0 +1,56 @@
+<template>
+    <form>
+        <div>
+          <label>FHB Clear Vinyl Stickers</label>
+          <input class="my-input">
+        </div>
+        <div>
+          <label>FHB Clear Vinyl Stickers</label>
+          <!-- Using the same "my-input" class on the -->
+          <!-- regular input above and on this container. -->
+          <div class="my-input" id="card-element"></div>
+        </div>
+      </form>
+</template>
+
+<script>
+
+cardElement.mount('#sticker-form');
+
+const appearance = {
+  theme: 'stripe',
+  variables: {
+    borderRadius: '36px',
+  }
+}
+
+const expressCheckoutOptions = {
+  buttonHeight: 50,
+  buttonTheme: {
+    applePay: 'white-outline'
+  }
+}
+
+const elements = stripe.elements({
+  mode: 'payment',
+  amount: 1099,
+  currency: 'usd',
+  appearance,
+})
+
+const expressCheckoutElement = elements.create(
+  'expressCheckout',
+  expressCheckoutOptions
+)
+
+expressCheckoutElement.mount('#express-checkout-element')
+
+</script>
+
+<style>
+    .my-input {
+        padding: 10px;
+        border: 1px solid #ccc;
+    }
+</style>
+
